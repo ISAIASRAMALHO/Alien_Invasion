35d952a12a7eaddd6f81084d2d0ce77b2fbdd7b8
ISAIASRAMALHO <isaiasramalhoepai@gmail.com>
2020-06-04 23:05:25 -0300

    Change

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,4 +1,18 @@
 import sys
 import pygame
 
-def run_game()
+def run_game():
+    pygame.init()
+    screen = pygame.display.set_mode( (1200, 600))
+    pygame.display.set_caption('Alien Invasion')
+    bg_color = (230, 230, 230)
+    # lOOP PRINCIPAL
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT: sys.exit()
+        screen.fill(bg_color)
+        pygame.display.flip()
+
+
+# LET'S RUN THE PROGRAMM/GAME
+run_game()
